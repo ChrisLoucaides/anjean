e65916388140998616f6ce9fc678e3ad130515e3
ChrisLoucaides <christosloucaides@gmail.com>
2023-10-28 00:58:47 +0100

    Introduced DlrService to pass in data as a child component

--- anjean/src/components/DlrService.vue
+++ anjean/src/components/DlrService.vue
@@ -1,13 +1,26 @@
 <template>
-
+  <div>
+    <h2>DLR Service Information</h2>
+    <div v-if="filteredData.length > 0">
+      <ul>
+        <li v-for="arrival in filteredData" :key="arrival.id">
+          <p><strong>Station Name:</strong> {{ arrival.stationName }}</p>
+          <p><strong>Destination Name:</strong> {{ arrival.destinationName }}</p>
+          <p><strong>Expected Departure Time:</strong> {{ arrival.expectedArrival }}</p>
+          <hr>
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <p>No arrivals found for Stratford International DLR Station</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "DlrService"
-}
+  props: {
+    filteredData: Array
+  }
+};
 </script>
-
-<style scoped>
-
-</style>
